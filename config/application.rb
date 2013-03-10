a95dac66fd95972de9a7768fb6e6430254baf45d
Rashmi Patel <rails@binary0110.co.uk>
2013-03-10 14:00:56 +0000

    Switch application.erb to haml

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module RailsCV
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    
     config.generators do |g|
         g.template_engine :haml
     end
